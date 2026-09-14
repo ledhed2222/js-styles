7d7d3a39778cbfc617ee38eebed91397ba3ae154
ledhed2222 <ledhed2222@users.noreply.github.com>
2026-09-14 19:12:09 -0400

    Simplify no-default-export override to cover all packages/*/*.js

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import base from '@ledhed2222/eslint-config'
 export default [
   ...base,
   {
-    files: ['packages/*/base.js', 'packages/*/typescript.js', 'packages/*/react.js', 'packages/*/prettier.js', 'packages/*/scss.js'],
+    files: ['packages/*/*.js'],
     rules: {
       'import/no-default-export': 'off',
     },
